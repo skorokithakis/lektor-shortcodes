96f59066c9fa644a0d388c4532b049fa25eb7a37
Stavros Korokithakis <stavros@korokithakis.net>
2017-05-19 13:34:57 +0300

    Fixes

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup
 
 setup(
     name='lektor-shortcodes',
-    version='0.1.2',
+    version='0.1.3',
     author=u'Stavros Korokithakis,,,',
     author_email='hi@stavros.io',
     url='https://github.com/skorokithakis/lektor-shortcodes',
     license='MIT',
-    py_modules=['lektor_shortcodes'],
+    packages=['lektor_shortcodes'],
+    keywords="lektor shortcodes",
     install_requires=[
     ],
     entry_points={
