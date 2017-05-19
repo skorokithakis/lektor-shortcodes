03fc453b4e949db1f615e7047334ae67f7170990
Stavros Korokithakis <stavros@korokithakis.net>
2017-05-19 12:58:33 +0300

    Update plugin structure

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lektor-shortcodes',
-    version='0.1.1',
+    version='0.1.2',
     author=u'Stavros Korokithakis,,,',
     author_email='hi@stavros.io',
     url='https://github.com/skorokithakis/lektor-shortcodes',
