535b1dfa13c2391277c0ac8770331941f6dfe954
Stavros Korokithakis <stavros@korokithakis.net>
2017-05-19 12:54:43 +0300

    Add URL

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup
 
 setup(
     name='lektor-shortcodes',
-    version='0.1',
+    version='0.1.1',
     author=u'Stavros Korokithakis,,,',
     author_email='hi@stavros.io',
+    url='https://github.com/skorokithakis/lektor-shortcodes',
     license='MIT',
     py_modules=['lektor_shortcodes'],
     install_requires=[
