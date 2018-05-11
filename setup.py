8c98160c234efa862c1b768ca298d9750a7f266e
Stavros Korokithakis <hi@stavros.io>
2018-05-11 23:29:42 +0300

    Add description

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     author=u'Stavros Korokithakis,,,',
     author_email='hi@stavros.io',
     url='https://github.com/skorokithakis/lektor-shortcodes',
+    description="The Lektor Shortcodes plugin allows you to use shortcodes (shortcodes are something like custom tags) in your fields (not templates), so your content doesn't have to have repetitive snippets over and over.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     license='MIT',
