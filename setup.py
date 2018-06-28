e0ee1ba92cba65f3d310be3490137a4eea284ad2
Stavros Korokithakis <hi@stavros.io>
2018-06-29 02:05:52 +0300

    Version 0.2.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="lektor-shortcodes",
-    version="0.2.4",
+    version="0.2.5",
     author=u"Stavros Korokithakis,,,",
     author_email="hi@stavros.io",
     url="https://github.com/skorokithakis/lektor-shortcodes",
